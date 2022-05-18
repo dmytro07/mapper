220bfad265556cf8e2f5fb772f7405116ccfffd3
dmytro_furtas <d.furtas@codelions.co>
2022-05-19 01:13:19 +0300

    Added multer

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  UploadedFiles,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileFieldsInterceptor } from '@nestjs/platform-express';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +15,36 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('upload')
+  @UseInterceptors(
+    FileFieldsInterceptor(
+      [
+        {
+          name: 'ard',
+          maxCount: 1,
+        },
+        { name: 'other' },
+      ],
+      {
+        fileFilter: (req, file, cb) => {
+          if (file.mimetype === 'application/pdf') {
+            return cb(null, true);
+          }
+          cb(new Error('Invalid mimetype'), false);
+        },
+        // storage:
+      },
+    ),
+  )
+  upload(
+    @UploadedFiles()
+    files: {
+      ard: Express.Multer.File[];
+      name: Express.Multer.File;
+    },
+  ) {
+    return files;
   }
 }
